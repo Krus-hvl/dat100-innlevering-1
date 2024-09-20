ee6a8f0222f99896efac904101d007a7e47233fd
Krus-hvl <186463@stud.hvl.no>
2024-09-20 15:19:34 +0200

    leverinn
    ferdig

--- src/no/hvl/dat100/O2.java
+++ src/no/hvl/dat100/O2.java
@@ -4,54 +4,46 @@ import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.*;
 
 public class O2 {
-	public static double beregnTrinnskatt(double inntekt) {
-        double skatt = 0;
-
-        // Trinn 5
-        if (inntekt > 1_350_000) {
-            skatt += (inntekt - 1_350_000) * 0.176;
-            inntekt = 1_350_000;
-        }
-
-        // Trinn 4
-        if (inntekt > 937_900) {
-            skatt += (inntekt - 937_900) * 0.166;
-            inntekt = 937_900;
-        }
-
-        // Trinn 3
-        if (inntekt > 670_000) {
-            skatt += (inntekt - 670_000) * 0.136;
-            inntekt = 670_000;
-        }
-
-        // Trinn 2
-        if (inntekt > 292_850) {
-            skatt += (inntekt - 292_850) * 0.04;
-            inntekt = 292_850;
-        }
-
-        // Trinn 1
-        if (inntekt > 208_050) {
-            skatt += (inntekt - 208_050) * 0.017;
-        }
-
-        return skatt;
-    }
-
-    public static void main(String[] args) {
-        try {
-            String input = showInputDialog(null, "Skriv inn din bruttoinntekt:", "Trinnskatt Kalkulator");
-
-            double bruttoinntekt = Double.parseDouble(input);
-
-            double trinnskatt = beregnTrinnskatt(bruttoinntekt);
-
-            showMessageDialog(null, String.format("Din trinnskatt er: %.2f kroner", trinnskatt), "Resultat");
-
-        } catch (NumberFormatException e) {
-        	showMessageDialog(null, "Vennligst skriv inn et gyldig tall for bruttoinntekt.", "Feil");
-        }
-    }
+	
+	public static void main(String[] args) {
+		
+		double skatteprosent1 = 1.7;
+		double skatteprosent2 = 4.0;
+		double skatteprosent3 = 13.6;
+		double skatteprosent4 = 16.6;
+		double skatteprosent5 = 17.6;
+		
+		
+		String inntektinn = showInputDialog("Skriv inn din inntekt for å regne skatt (venligst bruk heltall): ");
+		
+		int inntekt = Integer.parseInt(inntektinn);
+		
+		double skattbetale = 0;
+		
+		//trinn 1
+		if (inntekt >= 208051 && inntekt <= 292850) {
+			skattbetale = inntekt * (skatteprosent1 / 100);
+		}
+		//trinn 2
+		else if (inntekt >= 292851 && inntekt <= 670000) {
+			skattbetale = inntekt * (skatteprosent2 / 100);
+		}
+		//trinn 3
+		else if (inntekt >= 670001 && inntekt <= 937900) {
+			skattbetale = inntekt * (skatteprosent3 / 100);
+		}
+		//trinn 4
+		else if (inntekt >=  937901 && inntekt <= 1350000) {
+			skattbetale = inntekt * (skatteprosent4 / 100);
+		}
+		//trinn 5
+		else if (inntekt >= 1350001) {
+			skattbetale = inntekt * (skatteprosent5 / 100);
+		}
+		else {
+			showMessageDialog(null, "Ugyldig inntekt");
+		}
+		showMessageDialog(null, "Skatten du må betale er: " + skattbetale);
+		
+	}
 }
-
--- src/no/hvl/dat100/O3.java
+++ src/no/hvl/dat100/O3.java
@@ -1,8 +1,21 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.*;
+
 public class O3 {
 
 	public static void main(String[] args) {
 		
+			String nfakultetinn = showInputDialog("Skriv inn et heltall for n! (n > 0): ");
+			
+			int nfakultet = Integer.parseInt(nfakultetinn);
+			
+			int fakultet = 1;
+			for (int i = 1; i <= nfakultet; i++) {
+				fakultet *= i;
+				System.out.println(nfakultet);
+			}
+			showMessageDialog(null, nfakultetinn + "! = " + fakultet);
+			
 	}
 }
